6ee1e3788f53acba692738b309b65225e5b37cb6
Thibaut Delille <tdelille@gmail.com>
2022-10-04 16:17:46 -0700

    Created contaienr deploy script

---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,27 @@
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common.js');
+const packageJson = require ('../package.json');
+
+
+// add config to the prod config
+// to create a new bundle for marketing
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap'
+      },
+      shared: packageJson.dependencies
+    }),
+  ]
+}
+
+module.exports = merge(commonConfig, prodConfig);
