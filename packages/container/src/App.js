8b1981c4a7f155ba59320cc1ab911e616ca92f16
Thibaut Delille <tdelille@gmail.com>
2022-11-06 11:13:30 -0800

    generate production className

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,22 @@ import { BrowserRouter } from 'react-router-dom';
 
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
